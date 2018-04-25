03e2f702a641454232b744b11246fdc6fbd6c7f2
Trenton Vanderlinden <vande4tj@cmich.edu>
2018-04-25 11:04:35 +0100

    Tweaks to Master Files
    Fixes changes made when I made my first push.

--- src/GraphLink.java
+++ src/GraphLink.java
@@ -1,6 +1,5 @@
 public class GraphLink<T> {
 	public GraphNode<?> destNode;
-<<<<<<< HEAD
 	public double time;
 	public double length;
 	public int speed;
@@ -32,25 +31,7 @@ public class GraphLink<T> {
 		default:
 			this.speed = 25;
 			this.time = (length / speed) * 60;
-=======
-	public double cost;
-	private double length;
-	
-	public GraphLink(GraphNode<?> destNode, double length, char speed) {
-		this.destNode = destNode;
-		this.length = length;
-		switch(speed) {
-		case 'M':
-			this.cost = (this.length / 120) * 60; 
-			break;
-		case 'I':
-			this.cost = (this.length / 80) * 60; 
-			break;
-		case 'S':
-			this.cost = (this.length / 50) * 60;
-		default: 
->>>>>>> 713a5bb397fe8743530e7c614fe3638ef57f3d1a
-			break;
+
 		}
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
-<<<<<<< HEAD
 	public static void main(String[] args) throws FileNotFoundException {
 		CityList cities = new CityList();
 		for(int i = 0; i < cities.size(); i++) {
@@ -21,10 +20,6 @@ public class Main {
 			System.out.println(z.data);
 		}
 		System.out.printf("Total time: %.2f minutes.", quickest.pathCost);
-=======
-	public static void main(String[] args) {
-		
->>>>>>> 713a5bb397fe8743530e7c614fe3638ef57f3d1a
-	}
 
+	}
 }
