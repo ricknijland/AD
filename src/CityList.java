354c4fdc6441ccd92483a0044f387f67c2020a30
Trenton Vanderlinden <vande4tj@cmich.edu>
2018-04-26 23:59:23 +0100

    Removed some unneeded Imports I forgot to remove.

--- src/CityList.java
+++ src/CityList.java
@@ -1,10 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class CityList {
